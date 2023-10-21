684bafbe656f60ecf33df1aa1fb2f1c7447db5d8
andradean <viniciusansilva@outlook.com>
2023-10-21 14:54:12 -0300

    user entity constructor refactor

--- src/main/java/com/picpaysimplificado/domain/user/User.java
+++ src/main/java/com/picpaysimplificado/domain/user/User.java
@@ -1,7 +1,8 @@
 package com.picpaysimplificado.domain.user;
 
-
 import java.math.BigDecimal;
+
+import com.picpaysimplificado.dtos.UserDTO;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -14,31 +15,44 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity(name="users")
-@Table(name="users")
+@Entity(name = "users")
+@Table(name = "users")
 @Getter
 @Setter
 @AllArgsConstructor
-@EqualsAndHashCode(of="id")
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id; 
-    private String firstName; 
-    private String lastName; 
+    private Long id;
+    private String firstName;
+    private String lastName;
 
     @Column(unique = true)
     private String document;
 
-    @Column(unique = true) 
-    private String email; 
-   
+    @Column(unique = true)
+    private String email;
+
     private String password;
     private BigDecimal balance;
-    
+
     @Enumerated(EnumType.STRING)
     private UserType userType;
+
+    public User(UserDTO data) {
+        this.firstName = data.firstName();
+        this.lastName = data.lastName();
+        this.balance = data.balance();
+        this.email = data.email();
+        this.document = data.document();
+        this.password = data.password();
+        this.userType = data.userType();
+
+    }
 
 }
